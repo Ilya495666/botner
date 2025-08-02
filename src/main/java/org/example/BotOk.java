4bfe0817a2a187b00fe5f328b3461faa2fe73188
Ilya495666 <dolgihila14@gmail.com>
2025-08-02 21:30:18 +0300

    Add files via upload

--- src/main/java/org/example/BotOk.java
+++ src/main/java/org/example/BotOk.java
@@ -11,8 +11,7 @@ import java.sql.*;
 import java.util.*;
 
 public class BotOk extends TelegramLongPollingBot {
-    private static final String DB_URL = "jdbc:sqlite:kids_club.db";
-
+    private static final String DB_URL = System.getenv("DATABASE_URL"); // Используем PostgreSQL
     // Список chatId педагогов
     private static final Set<Long> TEACHER_IDS = Set.of(
             7241469346L, // Пример: твой ID
